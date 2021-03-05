edf9ff6d4bb735dc6107a5e500a03e6ee1b9b738
Makenna Garrett <kenna.dunford@gmail.com>
2021-03-05 10:55:50 -0700

    completed classes and closures sections.

--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -8,12 +8,20 @@
     myFunc should return getSecretString
   */
   
-  //CODE HERE
+  function myFunc(){
+    const myStr = 'super secret string'
+
+    function getSecretString(){
+      return myStr
+    }
+    return getSecretString
+  }
 
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
-  //CODE HERE
+  const secretString = myFunc()
+  // secretString()
   
   
   ////////////PROBLEM 2////////////
@@ -28,27 +36,40 @@
     The lightSwitch function should return flipTheSwitch.
   */
   
-  //CODE HERE
+  function lightSwitch(){
+    let isTheLightOn = false
+
+    function flipTheSwitch(){
+      if(isTheLightOn === false){
+        return isTheLightOn = true
+      }else {
+        return isTheLightOn = false
+      }
+    }
+    return flipTheSwitch
+  }
 
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
-  //CODE HERE
+  let kitchenSwitch = lightSwitch()
 
   
   //Invoke kitchenSwitch.
 
-  //CODE HERE
+  kitchenSwitch()
+  
   
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
-  //CODE HERE
+  let bathroomSwitch = lightSwitch()
   
   
   //Invoke bathroomSwitch twice.
   
-  //CODE HERE
+  bathroomSwitch()
+  bathroomSwitch()
 
   
   
@@ -63,7 +84,22 @@
       - 'prunePlant' should subtract 1 from the height and return the new height
   */
 
-  //CODE HERE
+  function plantTracker(){
+    let plant = 'fern'
+    let height = 12
+
+    return{
+      readInfo: function(){
+        return `This is a ${plant} plant that is ${height} inches tall.`
+      },
+      waterPlant: function(){
+        return height += 1
+      },
+      prunePlant: function(){
+        return height -= 1
+      }
+    }
+  }
 
 
   ////////////PROBLEM 4////////////
@@ -78,19 +114,37 @@
         - hint: try to find the index of the string first
   */
 
-  //CODE HERE
+  function inventory(){
+    let products = []
+
+    return{
+      readProducts: function(){
+        return products
+      },
+      addToProducts: function(item){
+        return products.push(item)
+      },
+      deleteFromProducts: function(item){
+        for(let i = 0; i < products.length; i++){
+          if(product[i] === item){
+            product.splice(i, 1)
+          }
+        }
+      }
+    }
+  }
 
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
   */
 
-  //CODE HERE
+  let shoes = inventory()
 
 
   /*
     Add an item to your shoes array using the addToProducts function
   */
 
-  //CODE HERE
+  shoes.addToProducts('shoes')
 
